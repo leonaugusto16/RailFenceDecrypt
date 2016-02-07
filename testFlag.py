ac5828850d57207388d57be8930c1d17ad9e2ad5
Leon Augusto <leonaugusto16@gmail.com>
2016-02-07 01:00:04 -0200

    update

--- testFlag.py
+++ testFlag.py
@@ -1,7 +1,6 @@
-from railfence import decrypt 
+from railfence import decrypt
 import sys
 
-cipher = "AaY--rpyfneJBeaaX0n-,ZZcs-uXeeSVJ-sh2tioaZ}slrg,-ciE-anfGt.-eCIyss-TzprttFliora{GcouhQIadctm0ltt-FYluuezTyorZ-"
-
+cipher = "WECRLTEERDSOEEFEAOCAIVDEN"
 for i in range(2,int(sys.argv[1])):
-	print "Key = %d"%i,decrypt(cipher,i,0);
+	print "key = %d"%i, decrypt(cipher,i,0);
