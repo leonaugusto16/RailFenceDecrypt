b23ac13fe3190cfa326987672c72236068e9dc4f
Leon Augusto <leonaugusto16@gmail.com>
2016-02-06 23:14:33 -0200

    tests begins

---
+++ testFlag.py
@@ -0,0 +1,4 @@
+from railfence import decrypt 
+
+for i in range(1,25):
+	print "Key = %d"%i,decrypt("AaY--rpyfneJBeaaX0n-,ZZcs-uXeeSVJ-sh2tioaZ}slrg,-ciE-anfGt.-eCIyss-TzprttFliora{GcouhQIadctm0ltt-FYluuezTyorZ-",i,0);
